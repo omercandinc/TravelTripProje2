9f1fafff6d3b17a0e63f8beac6ff94b73fb1e6c1
Omercan <omercandinc4152@hotmail.com>
2020-07-28 11:01:55 +0300

    Ana Sayfa tamamlandı

--- TravelTripProje/Controllers/BlogController.cs
+++ TravelTripProje/Controllers/BlogController.cs
@@ -10,15 +10,21 @@ namespace TravelTripProje.Controllers
     {
 		// GET: Blog
 		Context c = new Context();
-        public ActionResult Index()
+		BlogYorum by = new BlogYorum();
+		public ActionResult Index()
         {
-			var bloglar = c.Blogs.ToList();
-			return View(bloglar);
+			//var bloglar = c.Blogs.ToList();
+			by.Deger1= c.Blogs.ToList();
+			by.Deger3 = c.Blogs.OrderByDescending(x=>x.ID).Take(3).ToList();
+			return View(by);
         }
+		
 		public ActionResult BlogDetay(int id)
 		{
-			var blogbul = c.Blogs.Where(x => x.ID == id).ToList();
-			return View(blogbul);
+			by.Deger1= c.Blogs.Where(x => x.ID == id).ToList();
+			by.Deger2 = c.Yorumlars.Where(x => x.BlogID == id).ToList();
+			//var blogbul = c.Blogs.Where(x => x.ID == id).ToList();
+			return View(by);
 		}
 	}
 }
--- TravelTripProje/Controllers/DefaultController.cs
+++ TravelTripProje/Controllers/DefaultController.cs
@@ -3,20 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
 
 namespace TravelTripProje.Controllers
 {
     public class DefaultController : Controller
     {
-        // GET: Default
-        public ActionResult Index()
+		// GET: Default
+		Context c = new Context();
+		public ActionResult Index()
         {
-            return View();
+			var degerler = c.Blogs.ToList();
+			return View(degerler);
         }
 		public ActionResult About()
 		{
-			
 			return View();
+		}
+
+		public PartialViewResult Partial1()
+		{
+			var degerler = c.Blogs.OrderByDescending(x=> x.ID).Take(2).ToList();
+			return PartialView(degerler);
+		}
+		public PartialViewResult Partial2()
+		{
+			var degerler = c.Blogs.Where(x => x.ID==1).ToList();
+			return PartialView(degerler);
+		}
+		public PartialViewResult Partial3()
+		{
+			var degerler = c.Blogs.Take(10).ToList();
+			return PartialView(degerler);
+		}
+		public PartialViewResult Partial4()
+		{
+			var degerler = c.Blogs.Take(3).ToList();
+			return PartialView(degerler);
+		}
+		public PartialViewResult Partial5()
+		{
+			var degerler = c.Blogs.Take(3).OrderByDescending(x => x.ID).ToList();
+			return PartialView(degerler);
 		}
 	}
 }
